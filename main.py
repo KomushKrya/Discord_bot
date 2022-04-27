a4a63ca51eaafc872de073b0edf6fb8d44152a81
Komush <komushok357@gmail.com>
2022-04-27 08:35:33 +0700

    Промежуточная музыка

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import discord
-import logging
-import requests
+import os
 
+import discord
+from discord.ext import commands
+import logging
+import asyncio
+import youtube_dl
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -9,32 +12,95 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-TOKEN = "OTYxMjIwMjk4ODUyNjg3OTIy.Yk10KQ.kB4KlfbG86T2sYMqV6t2meeWRd8"
-
-
-class YLBotClient(discord.Client):
-    async def on_ready(self):
-        logger.info(f'{self.user} has connected to Discord!')
-        for guild in self.guilds:
-            logger.info(
-                f'{self.user} подключились к чату:\n'
-                f'{guild.name}(id: {guild.id})')
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        if "кот" in message.content.lower() or 'кош' in message.content.lower():
-            response = requests.get("https://api.thecatapi.com/v1/images/search").json()
-            cat = response[0]['url']
-            await message.channel.send(cat)
-        elif "пёс" in message.content.lower() or 'соба' in message.content.lower():
-            response = requests.get("https://dog.ceo/api/breeds/image/random").json()
-            dog = response['message']
-            await message.channel.send(dog)
-
-
 intents = discord.Intents.default()
 intents.members = True
-client = YLBotClient(intents=intents)
-client.run(TOKEN)
-print(123)
+server, server_id, channel_name = None, None, None
+domains = ['https://www.youtube.com', 'http://www.youtube.com', 'https://youtu.be', 'http://youtu.be']
+
+
+async def check_domains(link):
+    for x in domains:
+        if link.startswith('x'):
+            return True
+        return False
+
+
+class RandomThings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='play')
+    async def play(self, ctx, command=None):
+        global server, server_id, channel_name
+        source = None
+        author = ctx.author
+        if command is None:
+            server = ctx.guild
+            channel_name = author.voice.channel.name
+            voice_channel = discord.utils.get(server.voice_channels, name=channel_name)
+        params = command.split(' ')
+        if len(params) == 1:
+            source = params[0]
+            server = ctx.guild
+            channel_name = author.voice.channel.name
+            voice_channel = discord.utils.get(server.voice_channels, name=channel_name)
+        elif len(params) == 3:
+            server_id, voice_id, file_name = tuple(params)
+            try:
+                server_id = int(server_id)
+                voice_id = int(voice_id)
+            except ValueError:
+                await ctx.channel.send('Не робит')
+                return
+            server = bot.get_guild(server_id)
+            voice_channel = discord.utils.get(server.voice_channels, id=voice_id)
+        else:
+            await ctx.channel.send(f'Не робит, но 2')
+            return
+        print(1)
+        voice = discord.utils.get(bot.voice_clients, guild=server)
+        if voice is None:
+            await voice_channel.connect()
+            voice = discord.utils.get(bot.voice_clients, guild=server)
+        if source is None:
+            pass
+        elif source.startswith('http'):
+            if not check_domains(source):
+                await ctx.channel.send('Не робит, но 3')
+                return
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [
+                    {
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192'
+                    }
+                ]
+            }
+            mp3 = True
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                print(111, source)
+                ydl.download([source])
+                print(222)
+            for file in os.listdir('./'):
+                if file.endswith('.mp3'):
+                    os.rename(file, 'song.mp3')
+                if file.endswith('.webm'):
+                    os.rename(file, 'song.webm')
+                    mp3 = False
+            print(12345)
+            if mp3:
+                voice.play(discord.FFmpegPCMAudio('song.mp3'))
+            else:
+                voice.play(discord.FFmpegPCMAudio('song.webm'))
+        else:
+            voice.play(discord.FFmpegPCMAudio(f'music/{source}'))
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+bot.add_cog(RandomThings(bot))
+TOKEN = "OTYxMjIwMjk4ODUyNjg3OTIy.Yk10KQ.i8OGWo5bKIeSRoTA-JvHwLFKlxI"
+bot.run(TOKEN)
+
+#OTYxMjIwMjk4ODUyNjg3OTIy.Yk10KQ.i8OGWo5bKIeSRoTA-JvHwLFKlxI
