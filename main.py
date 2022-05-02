01381d0e55e7ac0c4a1d7e7f03f34e8db00d6675
Komush <komushok357@gmail.com>
2022-05-02 15:19:17 +0700

    Первые команды чата

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 from discord.ext import commands
 import logging
-from yandex_music import Client
-
+from database import DataBase
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -10,130 +9,99 @@ handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
+db = DataBase('discord_bot.db')
 intents = discord.Intents.default()
 intents.members = True
-domains = ['https://music.yandex.ru', 'http://music.yandex.ru']
-client = Client().init()
-server, server_id, channel_name = None, None, None
-queue = []
-start_playing = False
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-async def check_domains(link):
-    for x in domains:
-        if link.startswith(x):
-            return True
-        return False
+async def check_count(ctx):
+    global db
+    user_id = ctx.author.id
+    sql = f'SELECT counter FROM muted_users WHERE id = {user_id}'
+    result = db.select_with_fetchone(sql)
+    if not result:
+        db.query(f'INSERT INTO muted_users(id, counter) VALUES ({user_id}, {0})')
+        result = (0,)
+    result = result[0] + 1
+    db.query(f'UPDATE muted_users '
+             f'SET counter = {result} '
+             f'WHERE id = {user_id}')
+    if result % 3 == 0:
+        response = 'mute', result
+    else:
+        response = 'delete', result
+    return response
 
 
-async def add_queue(source):
-    if len(source) == 7:
-        album, track = source[4], source[6]
-        return [f'{track}:{album}']
-    elif len(source) == 5:
-        tracks = []
-        album = source[4]
-        album_song = client.albums_with_tracks(album)
-        for i, volume in enumerate(album_song.volumes):
-            for j in range(len(volume)):
-                tracks.append(f'{volume[j]["id"]}:{album}')
-        return tracks
+async def give_role(ctx, role_name):
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+    await discord.Member.add_roles(ctx.author, role)
 
 
-class MusicCommands(commands.Cog):
+async def mute(ctx):
+    await ctx.channel.purge(limit=1)
+    await give_role(ctx, 'Muted')
+
+
+class ChatCommands(commands.Cog):
     def __init__(self, ds_bot):
         self.bot = ds_bot
 
-    @commands.command(name='play')
-    async def play(self, ctx, command=None):
-        global server, server_id, channel_name, queue, start_playing
-        author = ctx.author
-        command = command.split(' ')
-        if len(command) != 1:
-            await ctx.channel.send(f'Команда имеет лишние данные')
+    @commands.command(name='srv')
+    async def srv(self, ctx, *, command):
+        """Позволяет создавать небольшие опросы"""
+        params = command.split('/')
+        if len(params) == 1:
+            text = command
+            color = int('ff0000', 16)
+            title = ''
+        elif len(params) == 2 or len(params) == 3:
+            if len(params) == 3:
+                title = params[2]
+            else:
+                title = ''
+            text = params[0]
+            if params[1] == '':
+                color = int('ff0000', 16)
+            else:
+                color = params[1]
+                try:
+                    color = int(color, 16)
+                except ValueError:
+                    await ctx.channel.send(f'{ctx.author.mention}, неправильно указан цвет')
+                    return
+        else:
+            await ctx.channel.send(f'{ctx.author.mention}, неправильно указаны параметры опроса')
             return
-        else:
-            source = command[0]
-            server = ctx.guild
-            channel_name = author.voice.channel.name
-        voice_channel = discord.utils.get(server.voice_channels, name=channel_name)
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        if voice is None:
-            await voice_channel.connect()
-            voice = discord.utils.get(bot.voice_clients, guild=server)
-        if source is None:
-            pass
-        elif source.startswith('http'):
-            if not await check_domains(source):
-                await ctx.channel.send('Неверно указана ссылка с сервера Яндекс.Музыки')
-                return
-            source = source.split('/')
-            queue += await add_queue(source)
-            print(queue, 123)
-            if not start_playing:
-                start_playing = True
-                while True:
-                    if queue and not voice.is_playing():
-                        print(2)
-                        song = [queue.pop(0)]
-                        print(song, 321)
-                        client.tracks(song)[0].download('song.mp3', bitrate_in_kbps=128)
-                        voice.play(discord.FFmpegPCMAudio('song.mp3'))
-                    if not queue and not voice.is_playing():
-                        start_playing = False
-        else:
-            await ctx.channel.send('Неверный протокол')
-            return
+        await ctx.message.delete()
 
-    @commands.command(name='pause')
-    async def pause(self, ctx):
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        if voice.is_playing():
-            voice.pause()
-        else:
-            await ctx.channel.send('Музыка уже приостановлена')
-
-    @commands.command(name='resume')
-    async def resume(self, ctx):
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        if voice.is_playing():
-            await ctx.channel.send('Музыка уже воспроизводится')
-        else:
-            voice.resume()
-
-    @commands.command(name='stop')
-    async def stop(self):
-        global queue, start_playing
-        queue, start_playing = [], False
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        voice.stop()
-
-    @commands.command(name='leave')
-    async def leave(self, ctx):
-        global server, channel_name, queue, start_playing
-        queue, start_playing = [], False
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        if voice.is_connected():
-            queue, start_playing = [], False
-            await voice.disconnect()
-        else:
-            await ctx.channel.send('Бот не находится в голосовом канале')
-
-    @commands.command(name='skip')
-    async def leave(self, ctx):
-        global server, channel_name, queue, start_playing
-        queue.pop()
-        voice = discord.utils.get(bot.voice_clients, guild=server)
-        if voice.is_connected():
-            queue, start_playing = [], False
-            await voice.disconnect()
-        else:
-            await ctx.channel.send('Бот не находится в голосовом канале')
+        msg = await ctx.channel.send(embed=discord.Embed(title=title, description=text, color=color))
+        await msg.add_reaction('☑')
+        await msg.add_reaction('❌')
 
 
-bot.add_cog(MusicCommands(bot))
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+    author = ctx.author
+    with open('ban_words.txt', encoding='utf8') as bw:
+        for word in bw:
+            word = word.strip()
+            if word in ctx.content.lower():
+                response = await check_count(ctx)
+                msg, counter = response
+                if msg == 'delete':
+                    await ctx.delete()
+                    await ctx.channel.send(f'{author.mention}, не используйте запрещенные слова!')
+                else:
+                    await mute(ctx)
+                    await ctx.channel.send(f'Пользователю {author.mention} был выдан мут из-за использования '
+                                           f'запрещенных слов!')
+    await bot.process_commands(ctx)
+
+bot.add_cog(ChatCommands(bot))
 TOKEN = "OTYxMjIwMjk4ODUyNjg3OTIy.Yk10KQ.uhwFKqUvMunyou7w8jYKo2lkrdg"
 bot.run(TOKEN)
 # OTYxMjIwMjk4ODUyNjg3OTIy.Yk10KQ.uhwFKqUvMunyou7w8jYKo2lkrdg
